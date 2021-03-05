964d62e6f8f8048b7ab5722e965f70ce0a25b6f5
benjaminglt1 <59864076+benjaminglt1@users.noreply.github.com>
2021-03-05 09:34:40 +0100

    ajout sauvegarde des données + sécurité api (token)

--- API_Payement/serveurRest.js
+++ API_Payement/serveurRest.js
@@ -14,11 +14,7 @@ function genererToken(){
     var tok;
     var end = false;
     while(!end){
-        tok = strRandom({
-            includeUpperCase: true,
-            includeNumbers: true,
-            length: 10
-        });
+        tok = Str.random(10);
         if(token.indexOf(tok) == -1){
             token.push(tok);
             end = true;
@@ -35,36 +31,47 @@ function supprToken(tok){
 }
 
 const clients = require('./clients.json');
-const crediteurs = require('./crediteurs.json');
-
+const vendeurs = require('./vendeurs.json');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
+const Str = require('@supercharge/strings');
+
+app.use(express.json());
 
 //Se connecter à l'api / obtenir un token
 app.post('/api/login', (requete,reponse) => {
-    var log = requete.params.log;
-    var pass = requete.params.pass;
-    var tok = genererToken();
-    crediteurs.find(cred => cred.login === log && cred.mdp === pass).token = tok;
-
-    var rep = JSON.stringify({
-        status: "ok",
-        token: tok
-    });
-    reponse.status(200).json(rep);
+    var log = requete.body.login;
+    var pass = requete.body.pass;
+    if(vendeurs.indexOf(vendeurs.find(cred=>cred.login===log && cred.mdp===pass))>-1){
+        var tok = genererToken();
+        var rep = JSON.stringify({
+            status: "ok",
+            token: tok
+        });
+        reponse.status(200).json(rep);
+    }else{
+        var rep = JSON.stringify({
+            status: "Mauvais identifiants"
+        });
+        reponse.status(401).json(rep);
+    }
+    
 });
 
 //Se deconnecter / supprimer le token
-app.post('/api/logout', (requete,reponse) => {
-    supprToken(requete.body.token);
-    crediteurs.find(cred => cred.token === requete.body.token).token = "";
-    var rep = JSON.stringify({
-        status: "ok"
-    });
-    reponse.status(200).json(rep);
+app.get('/api/logout', (requete,reponse) => {
+    if(token.indexOf(requete.headers.token)>-1){
+        supprToken(requete.headers.token);
+        var rep = JSON.stringify({
+            status: "ok"
+        });
+        reponse.status(200).json(rep);
+    }else{
+        reponse.status(401).json({"status":"Il faut se connecter"});
+    }
 });
 
 
@@ -72,55 +79,114 @@ app.post('/api/logout', (requete,reponse) => {
 
 //Voir les clients
 app.get('/api/clients', (requete,reponse) => {
-    reponse.status(200).json(clients);
+    if(token.indexOf(requete.headers.token)>-1){
+        reponse.status(200).json(clients);
+    }else{
+        reponse.status(401).json({"status":"Il faut se connecter"});
+    }
 });
 
 //Voir un client
 app.get('/api/client/:id', (requete,reponse) => {
-    var id = parseInt(requete.params.id);
-    var client = clients.find(client => client.id === id);
-    reponse.status(200).json(client);
+    if(token.indexOf(requete.headers.token)>-1){
+        var id = parseInt(requete.params.id);
+        var client = clients.find(client => client.id === id);
+        reponse.status(200).json(client);
+    }else{
+        reponse.status(401).json({"status":"Il faut se connecter"});
+    }
 });
 
 //Creer client
 app.post('/api/client', (requete,reponse) => {
-    clients.push(requete.body);
-    reponse.status(200).json(clients);
+    if(token.indexOf(requete.headers.token)>-1){
+        clients.push(requete.body);
+
+        fs.writeFile('clients1.json', JSON.stringify(clients), (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("JSON sauvegardé");
+        });
+
+        reponse.status(200).json(clients);
+    }else{
+        reponse.status(401).json({"status":"Il faut se connecter"});
+    }
 });
 
 //ajouter carte
 app.post('/api/client/:id/ajouterCarte', (requete,reponse) => {
-    var id = parseInt(requete.params.id);
-    clients.find(client => client.id === id).carte.push(requete.body);
-    reponse.status(200).json(clients);
+    if(token.indexOf(requete.headers.token)>-1){
+        var id = parseInt(requete.params.id);
+        clients.find(client => client.id === id).carte.push(requete.body);
+        
+        fs.writeFile('clients1.json', JSON.stringify(clients), (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("JSON sauvegardé");
+        });
+
+        reponse.status(200).json(clients);
+    }else{
+        reponse.status(401).json({"status":"Il faut se connecter"});
+    }
 });
 
 //supprimer carte
 app.delete('/api/client/:id/supprimerCarte/:nom', (requete,reponse) => {
-    var id = parseInt(requete.params.id);
-    var num = parseInt(requete.params.nom);
-    
-    var d = clients.find(client => client.id === id).carte.findIndex(c=> c.numero === num);
-    console.log(d);
-    if(d>=0){
-    clients.find(client => client.id === id).carte.splice(d,1);
+    if(token.indexOf(requete.headers.token)>-1){
+        var id = parseInt(requete.params.id);
+        var num = parseInt(requete.params.nom);
+        
+        var d = clients.find(client => client.id === id).carte.findIndex(c=> c.numero === num);
+        console.log(d);
+        if(d>=0){
+            clients.find(client => client.id === id).carte.splice(d,1);
+        }
+        
+        fs.writeFile('clients1.json', JSON.stringify(clients), (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("JSON sauvegardé");
+        });
+        
+        reponse.status(200).json(clients);
+    }else{
+        reponse.status(401).json({"status":"Il faut se connecter"});
     }
-    reponse.status(200).json(clients);
 });
 
 //Voir operations
 app.get('/api/client/:id/operations', (requete,reponse) => {
-    var id = parseInt(requete.params.id);
-    var client = clients.find(client => client.id === id);
-    reponse.status(200).json(client.operations);
+    if(token.indexOf(requete.headers.token)>-1){
+        var id = parseInt(requete.params.id);
+        var client = clients.find(client => client.id === id);
+        reponse.status(200).json(client.operations);
+    }else{
+        reponse.status(401).json({"status":"Il faut se connecter"});
+    }
 });
 
 //Simuler payement
 app.post('/api/simPayement/:idClient', (requete,reponse) => {
-    var id = parseInt(requete.params.idClient);
-    clients.find(client => client.id === id).operations.push(requete.body);
-
-    reponse.status(200).json(requete.body);
+    if(token.indexOf(requete.headers.token)>-1){
+        var id = parseInt(requete.params.idClient);
+        clients.find(client => client.id === id).operations.push(requete.body);
+        
+        fs.writeFile('clients1.json', JSON.stringify(clients), (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("JSON sauvegardé");
+        });
+        
+        reponse.status(200).json(requete.body);
+    }else{
+        reponse.status(401).json({"status":"Il faut se connecter"});
+    }
 });
 
 
